c3689a004192186880d8e48935febb37c349fdd1
amccalli <drewm.mccallister@gmail.com>
2018-01-30 18:17:39 -0500

    Add files via upload

--- Drew_McCallister_HW2_Q1_c.py
+++ Drew_McCallister_HW2_Q1_c.py
@@ -9,12 +9,13 @@ Created on Sun Jan 28 13:17:30 2018
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+#Test odd man out of N coins
 N=5
-Runs=100000
+Runs=1000
 Success=np.zeros(Runs)
 N_Successes=np.zeros(Runs)
 for x in range(Runs) :
+    #counts number flips it takes to get success, output: average
     flip=0;
     for y in range(N*5) :
         Coins=np.random.randint(0,2,N)
@@ -23,18 +24,21 @@ for x in range(Runs) :
             Success[x]=flip
             #print(Coins)
             break
+    #counts number of success after number of flips, output: N_successes    
     counter=0;
-#    for z in range(x) :
-#        Coins=np.random.randint(0,2,N)
-#        if (np.sum(Coins)==(N-1) or np.sum(Coins)==1) :
-#            counter=counter+1
-#    N_Successes[x]=counter               
+    for z in range(x) :
+        Coins=np.random.randint(0,2,N)
+        if (np.sum(Coins)==(N-1) or np.sum(Coins)==1) :
+            counter=counter+1
+    N_Successes[x]=counter               
 average=np.average(Success)
-#plt.plot(N_Successes)
+plt.plot(N_Successes)
 
+
+#Test with one biased coin, probabiilty B
 N2=5
 B=0.9
-Runs2=100000
+Runs2=1000
 Success2=np.zeros(Runs)
 for x in range(Runs) :
     flip=0;
